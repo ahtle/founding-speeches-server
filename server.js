513e1cbf3c91c9694e4be06815aa11715066ace0
anhhtle <anh.ht.le@gmail.com>
2017-06-13 22:00:35 -0700

    new

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.get('/', (req, res) => {
 });
 
 // get all presidents
-app.get('/presidents', (req, res) => {
+app.get('/api/v1/presidents', (req, res) => {
     Presidents.find().exec().then(presidents => {
         console.log(presidents);
         res.status(200).json(presidents);
@@ -53,7 +53,7 @@ function validatePost(data){
     return true;
 };
 
-app.post('/presidents', (req, res) => {
+app.post('/api/v1/presidents', (req, res) => {
     if(validatePost(req)){
         const newPres = {
             presId: req.body.presId,
@@ -78,7 +78,7 @@ app.post('/presidents', (req, res) => {
 })
 
 // get all president transcripts
-app.get('/transcripts/:presId', (req, res) => {
+app.get('api/v1/transcripts/:presId', (req, res) => {
     Transcripts.find({presId: req.params.presId}).sort({date: -1}).exec().then(transcripts => {
         console.log(transcripts);
         res.status(200).json(transcripts);
@@ -101,7 +101,7 @@ function validateTranscriptPost(data){
     return true;
 };
 
-app.post('/transcripts', (req, res) => {
+app.post('/api/v1/transcripts', (req, res) => {
     if(validateTranscriptPost(req)){
         const newTranscript = {
             presId: req.body.presId,
@@ -124,7 +124,7 @@ app.post('/transcripts', (req, res) => {
 
 /************** get Watson profile *****************/
 
-app.post('/watson', (req, res) => {
+app.post('/api/v1/watson', (req, res) => {
     var personality_insights = new PersonalityInsightsV3({
         username: '507f3d3b-10a0-4cba-a409-423da0bf5915',
         password: '8cSh3iwYj8l1',
@@ -144,8 +144,15 @@ app.post('/watson', (req, res) => {
             console.error(err);
         else{
             //let output = JSON.stringify(response, null, 2);
+            console.log(response);
             let personality = response.personality.map((obj) => {
-                return {"name": obj.name, "percentile": (100*obj.percentile).toFixed(0)}
+                return {
+                    "name": obj.name,
+                    "percentile": (100*obj.percentile).toFixed(0),
+                    "children": obj.children.map(children => {
+                        return {"name": children.name, "percentile": children.percentile}
+                    }).sort((a,b) => {return b.percentile - a.percentile})
+                }
             });
             let needs = response.needs.map((obj) => {
                 return {"name": obj.name, "percentile": (100*obj.percentile).toFixed(0)}
@@ -155,9 +162,10 @@ app.post('/watson', (req, res) => {
             });
 
             let outputFormatted = {
-                personality: personality,
-                needs: needs,
-                values: values
+                word_count: response.word_count,
+                personality: personality.sort((a,b) => {return b.percentile - a.percentile}),
+                needs: needs.sort((a,b) => {return b.percentile - a.percentile}),
+                values: values.sort((a,b) => {return b.percentile - a.percentile})
             }
 
             return res.status(200).send(outputFormatted);
